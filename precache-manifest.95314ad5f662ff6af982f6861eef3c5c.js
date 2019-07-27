793c3b8eb2bdb0f3134869e0e9c4327afb34e93a
kenneth <knnthcrtl.kc@gmail.com>
2019-07-27 22:14:14 +0800

    Updates

---
+++ precache-manifest.95314ad5f662ff6af982f6861eef3c5c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab81476485b0697ff8237d4e8e57bf4",
+    "url": "/kinect/index.html"
+  },
+  {
+    "revision": "5d6289b21c8be53faba2",
+    "url": "/kinect/static/css/3.d432a4d4.chunk.css"
+  },
+  {
+    "revision": "1e0c3b587d1c82ed01d9",
+    "url": "/kinect/static/css/4.0156f633.chunk.css"
+  },
+  {
+    "revision": "cc4990cdac99aba0d50b",
+    "url": "/kinect/static/css/5.df62bbc8.chunk.css"
+  },
+  {
+    "revision": "632cbc889fbadbf1d3e9",
+    "url": "/kinect/static/css/6.fc028217.chunk.css"
+  },
+  {
+    "revision": "94f00e603e87678071ec",
+    "url": "/kinect/static/css/7.631fda43.chunk.css"
+  },
+  {
+    "revision": "9a0bb48fdcc64223bdc9",
+    "url": "/kinect/static/css/8.cbd4e793.chunk.css"
+  },
+  {
+    "revision": "c9721097b9d851428bfd",
+    "url": "/kinect/static/css/9.e95a3574.chunk.css"
+  },
+  {
+    "revision": "7e77c78323e0197629a9",
+    "url": "/kinect/static/css/main.99481666.chunk.css"
+  },
+  {
+    "revision": "4c2cbac85dfb7344c4ae",
+    "url": "/kinect/static/js/2.78f5810d.chunk.js"
+  },
+  {
+    "revision": "5d6289b21c8be53faba2",
+    "url": "/kinect/static/js/3.b726e62a.chunk.js"
+  },
+  {
+    "revision": "1e0c3b587d1c82ed01d9",
+    "url": "/kinect/static/js/4.5d836531.chunk.js"
+  },
+  {
+    "revision": "cc4990cdac99aba0d50b",
+    "url": "/kinect/static/js/5.9ae4971b.chunk.js"
+  },
+  {
+    "revision": "632cbc889fbadbf1d3e9",
+    "url": "/kinect/static/js/6.e3cef44f.chunk.js"
+  },
+  {
+    "revision": "94f00e603e87678071ec",
+    "url": "/kinect/static/js/7.d6463564.chunk.js"
+  },
+  {
+    "revision": "9a0bb48fdcc64223bdc9",
+    "url": "/kinect/static/js/8.6f4391b7.chunk.js"
+  },
+  {
+    "revision": "c9721097b9d851428bfd",
+    "url": "/kinect/static/js/9.381d397d.chunk.js"
+  },
+  {
+    "revision": "7e77c78323e0197629a9",
+    "url": "/kinect/static/js/main.01783d2d.chunk.js"
+  },
+  {
+    "revision": "507532b37d4b7d0d3c30",
+    "url": "/kinect/static/js/runtime~main.4affdf3d.js"
+  },
+  {
+    "revision": "e570262b45a44a4fcabf2831a63b2baf",
+    "url": "/kinect/static/media/agent_skill.e570262b.jpg"
+  },
+  {
+    "revision": "78059ffbd10f9b3bd35b03a0127c68c4",
+    "url": "/kinect/static/media/agent_skill_assignment.78059ffb.jpg"
+  },
+  {
+    "revision": "46a7d48240d428c9dc3d4ff579199312",
+    "url": "/kinect/static/media/poppins.46a7d482.woff"
+  }
+]);
